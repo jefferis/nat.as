d0622d1a35c97c48a053b39c4b75fea553d834e0
Gregory Jefferis <jefferis@gmail.com>
2014-01-02 23:10:30 +0000

    Tidy package documentation

--- R/nat.as-package.R
+++ R/nat.as-package.R
@@ -1,18 +1,28 @@
 #' Analyse 3D biological image data especially neurons
 #' 
-#' Key functions that you will want to look at include
-#' read.neuron
-#' write.neuron
+#' Before you can use the main functionality of this package, you must install 
+#' the https://github.com/jefferis/AnalysisSuite codebase using 
+#' \code{\link{install_analysis_suite}}.
 #' 
+#' In future you can use \code{load_analysis_suite()} or
+#' \code{require_analysis_suite()} to load the codebase in your own code.
+#' 
+#' @details Key functions within AnalysisSuite that you will want to look at
+#'   include
+#' \itemize{
+#' \item{read.neuron}
+#' \item{write.neuron}
+#' \item{plot.neuron}
+#' \item{plot3d.neuron}
+
 #' Objects of class
-#' neuron
-#' neuronlist
-#' 
-#' plot.neuron
-#' plot3d.neuron
+#' \item{neuron}
+#' \item{neuronlist}
+#' }
 #' @name nat.as-package
 #' @aliases nat.as
-#' @seealso \code{\link[rgl]{rgl-package}} which is used for visualisation.
+#' @seealso \code{\link{load_analysis_suite},\link{require_analysis_suite}} and
+#'   \code{\link[rgl]{rgl-package}} which is used for visualisation.
 #' @docType package
 #' @keywords package
 NULL
