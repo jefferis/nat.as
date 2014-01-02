5b3ed9a4081c752c907a6d6259978b4d65f5267e
Gregory Jefferis <jefferis@gmail.com>
2014-01-02 23:28:30 +0000

    Normalise path to zip file

--- R/install.R
+++ R/install.R
@@ -35,7 +35,7 @@ download_zip<-function(zip_url,zip_dir='.',zip_file=NULL){
     zip_file=sub(".*filename=",'',h$headers[['content-disposition']])
   }
   
-  zip_path=file.path(zip_dir,zip_file)  
+  zip_path=normalizePath(file.path(zip_dir,zip_file))
   request=GET(zip_url, config(ssl.verifypeer = FALSE))
   stop_for_status(request)
   writeBin(content(request), zip_path)
