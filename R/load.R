018c7b9228d6e8c8b1a8d7b58c1169c55200dce7
Gregory Jefferis <jefferis@gmail.com>
2014-01-03 22:14:50 +0000

    remove extraneous load functions and make .require_analysis_suite() internal
    * require_analysis_suite should not be called directly because it is
      designed for use inside the .onLoad function.
    * calling it directly would load code into the global namespace

--- R/load.R
+++ R/load.R
@@ -1,35 +1,16 @@
-
-#' @details
-#' \describe{
-#'  \item{load_analysis_suite}{load and error out on failure} 
-#'  \item{require_analysis_suite}{try to load and return logical indicating
-#'   success}
-#'  \item{installnload_analysis_suite}{install if required, then load
-#'   and error out on failure}
-#' }
-#' @export
-load_analysis_suite<-function(){
-  if(!require_analysis_suite()) stop("Unable to start AnalysisSuite")
-}
-
-#' Start AnalysisSuite by sourcing all function definitions
+#' nat.as internal functions
 #' 
-#' This is designed to be for developer use only.
-#' @export
-#' @rdname load_analysis_suite
-require_analysis_suite<-function(){
+#' This function (which is designed for internal use only) will Start 
+#' AnalysisSuite by sourcing all function definitions. Users should load the
+#' code with \code{library(nat.as)} or \code{reload_analysis_suite()}.
+#' 
+#' @rdname nat.as-internal
+#' @seealso \code{\link{reload_analysis_suite}}
+.require_analysis_suite<-function(){
   startup=file.path(analysis_suite_dir(),'R','Code','Startup.R')
   if(!file.exists(startup)) return(FALSE)
   t=try(sys.source(startup,envir=topenv(),keep.source=TRUE),silent=TRUE)
   success<-!inherits(t,"try-error")
-}
-
-#' @rdname load_analysis_suite
-#' @param ... Additional arguments passed to install_analysis_suite
-#' @seealso \code{\link{install_analysis_suite}}
-installnload_analysis_suite<-function(...){
-  if(!require_analysis_suite()) install_analysis_suite(...)
-  if(!require_analysis_suite()) stop("Failed to install and load AnalysisSuite")
 }
 
 #' Return path to root of AnalysisSuite package
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  if(!require_analysis_suite() && interactive())
+  if(!.require_analysis_suite() && interactive())
     packageStartupMessage('Please use nat.as::install_analysis_suite()',
                           ' to install the main AnalysisSuite codebase!')
   invisible()
