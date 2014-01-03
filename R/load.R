08f3d2ea3a7b5d8753e58d6dcc07839f64bbb1d9
Gregory Jefferis <jefferis@gmail.com>
2014-01-03 16:12:17 +0000

    Move load functions from install.R into separate load.R

---
+++ R/load.R
@@ -0,0 +1,34 @@
+#' Start AnalysisSuite by sourcing all function definitions
+#' @details
+#' \describe{
+#'  \item{load_analysis_suite}{load and error out on failure} 
+#'  \item{require_analysis_suite}{try to load and return logical indicating
+#'   success}
+#'  \item{installnload_analysis_suite}{install if required, then load
+#'   and error out on failure}
+#' }
+#' @export
+load_analysis_suite<-function(){
+  if(!require_analysis_suite()) stop("Unable to start AnalysisSuite")
+}
+
+#' @export
+#' @rdname load_analysis_suite
+require_analysis_suite<-function(){
+  t=try(source(file.path(analysis_suite_dir(),'R','Code','Startup.R')))
+  success<-!inherits(t,"try-error")
+}
+
+#' @rdname load_analysis_suite
+#' @param ... Additional arguments passed to install_analysis_suite
+#' @seealso \code{\link{install_analysis_suite}}
+installnload_analysis_suite<-function(...){
+  if(!require_analysis_suite()) install_analysis_suite(...)
+  if(!require_analysis_suite()) stop("Failed to install and load AnalysisSuite")
+}
+
+#' Return path to root of AnalysisSuite package
+#' @export
+analysis_suite_dir<-function(){
+  file.path(system.file(package="nat.as"),"AnalysisSuite")
+}
