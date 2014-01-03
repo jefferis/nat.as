b245d45e2f057afd7f6c8f34326e11b2739af524
Gregory Jefferis <jefferis@gmail.com>
2014-01-03 22:02:14 +0000

    update package docs
    * nb also includes @exportPattern roxygen tag

--- R/nat.as-package.R
+++ R/nat.as-package.R
@@ -2,13 +2,19 @@
 #' 
 #' Before you can use the main functionality of this package, you must install 
 #' the https://github.com/jefferis/AnalysisSuite codebase using 
-#' \code{\link{install_analysis_suite}}.
+#' \code{\link{install_analysis_suite}}. The package README on github contains 
+#' details:
 #' 
-#' In future you can use \code{load_analysis_suite()} or
-#' \code{require_analysis_suite()} to load the codebase in your own code.
+#' https://github.com/jefferis/nat.as
+#' 
+#' In future you can simply use \code{library(nat.as)} or \code{require(nat.as)}
+#' to load the full AnalysisSuite codebase. Note that the functions will loaded
+#' into the nat.as namespace and then exported to avoid polluting the global
+#' namespace. If you are hacking AnalysisSuite, you can use \code{ 
+#' reload_analysis_suite() } to reload code.
 #' 
 #' @details Key functions within AnalysisSuite that you will want to look at
-#'   include
+#'   include:
 #' \itemize{
 #' \item{read.neuron}
 #' \item{write.neuron}
@@ -25,4 +31,5 @@
 #'   \code{\link[rgl]{rgl-package}} which is used for visualisation.
 #' @docType package
 #' @keywords package
+#' @exportPattern "^[^\\.]"
 NULL
