4380e99b8956570f1ae10102cdb5eb7fb1eb9c40
Gregory Jefferis <jefferis@gmail.com>
2014-01-03 21:16:41 +0000

    automatically load AnalysisSuite code on package load

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onLoad <- function(libname, pkgname) {
+  if(!require_analysis_suite() && interactive())
+    packageStartupMessage('Please use nat.as::install_analysis_suite()',
+                          ' to install the main AnalysisSuite codebase!')
+  invisible()
+}
